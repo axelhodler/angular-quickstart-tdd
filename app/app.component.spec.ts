e84d1b067bafd4c640cee4412eff4bb34dba4c26
Axel Hodler <derxor@gmail.com>
2016-11-06 18:55:14 +0100

    refactor tests

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -6,6 +6,13 @@ import {Hero} from './hero';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {By}           from '@angular/platform-browser';
 
+
+function queryFirstHeading(fixture: ComponentFixture<AppComponent>) {
+  return fixture.debugElement.query(By.css('h1')).nativeElement;
+}
+function querySecondHeading(fixture: ComponentFixture<AppComponent>) {
+  return fixture.debugElement.query(By.css('h2')).nativeElement;
+}
 describe('AppComponent', function () {
   let firstHeading: any;
   let secondHeading: any;
@@ -15,7 +22,7 @@ describe('AppComponent', function () {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [ FormsModule ]
+      imports: [FormsModule]
     })
       .compileComponents();
   }));
@@ -23,40 +30,38 @@ describe('AppComponent', function () {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
-    firstHeading = fixture.debugElement.query(By.css('h1')).nativeElement;
-    secondHeading = fixture.debugElement.query(By.css('h2')).nativeElement;
     fixture.detectChanges();
   });
 
   it('sets the title to Tour of Heroes', () => {
-    expect(firstHeading.innerText).toMatch(/Tour of Heroes/i);
+    expect(queryFirstHeading(fixture).innerText).toMatch(/Tour of Heroes/i);
     expect(comp.title).toBe('Tour of Heroes');
   });
 
   it('gives the hero a name', () => {
     expect(comp.hero.name).toBe('Windstorm');
-    expect(secondHeading.innerText).toBe('Windstorm details!');
+    expect(querySecondHeading(fixture).innerText).toBe('Windstorm details!');
   });
 
   it('displays h2 after h1', () => {
-    expect(firstHeading.nextElementSibling.textContent).toBe('Windstorm details!');
+    expect(queryFirstHeading(fixture).nextElementSibling.textContent).toBe('Windstorm details!');
   });
 
   it('displays hero id and name', () => {
-    const heroIdElement = secondHeading.nextElementSibling;
+    const heroIdElement = querySecondHeading(fixture).nextElementSibling;
     expect(heroIdElement.textContent).toContain('id:');
     expect(heroIdElement.lastChild.textContent).toBe('1');
   });
 
   it('can edit the heroes name', () => {
-    let heroIdElement = secondHeading.nextElementSibling;
+    let heroIdElement = querySecondHeading(fixture).nextElementSibling;
     comp.hero.name = 'Stormwind';
     fixture.detectChanges();
     expect(heroIdElement.nextElementSibling.lastElementChild.outerHTML).toContain('Stormwind');
   });
 
   it('lists ten heroes with name and id', () => {
-    let heroList = secondHeading.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+    let heroList = querySecondHeading(fixture).nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
     expect(heroList.childElementCount).toBe(10);
     expect(heroList.firstElementChild.textContent).toContain('11 Mr. Nice');
   });
